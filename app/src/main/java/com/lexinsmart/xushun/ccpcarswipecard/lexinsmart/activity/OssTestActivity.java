80bf36b037caf3efd5fd669788b02cf5737af4cc
shun1249844726 <1249844726@qq.com>
2017-11-06 18:15:54 +0800

    准备做蓝牙的重连， 刷卡记录上传。

---
+++ app/src/main/java/com/lexinsmart/xushun/ccpcarswipecard/lexinsmart/activity/OssTestActivity.java
@@ -0,0 +1,19 @@
+package com.lexinsmart.xushun.ccpcarswipecard.lexinsmart.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xushun on 2017/11/6.
+ * 功能描述：
+ * 心情：
+ */
+
+public class OssTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.lexinsmart.xushun.ccpcarswipecard.R.layout.activity_osstest);
+    }
+}
